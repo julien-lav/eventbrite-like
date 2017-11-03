938b3546d42dc9495f5a3879326c0000b04518aa
julien-lav <julien.laville@hotmail.fr>
2017-11-03 17:10:02 +0100

    start user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
-	has_and_belongs_to_many :events
+	has_many :authored_events, :foreign_key => :creator_id, :class_name => 'Event'
+	has_and_belongs_to_many :attended_events, :class_name => 'Event', :join_table => :events_users
 end
